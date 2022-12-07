cb5f0267712995435b43a988af97073d11964ee7
ExistHyper <jan.kordas90@gmail.com>
2022-12-07 17:16:02 +0100

    dodano ls

--- microshell.c
+++ microshell.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 #include <linux/limits.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <signal.h>
 
 char commands[40];
 void get_current_dir();
 void exit_function();
 void unknown_command_error();
 void help_option();
+void display_files();
 
 int main(){
     char hostname[_SC_HOST_NAME_MAX];
@@ -26,13 +30,14 @@ int main(){
         exit_function();
         unknown_command_error();
         help_option();
+        display_files();
 
         //printf("%s", commands);
         //printf("\n%d\n", strcmp(commands,"exit"));
     }
 }
 void unknown_command_error(){
-    if(strcmp(commands,"pwd") != 10 && strcmp(commands,"exit") != 10 && strcmp(commands,"help") != 10){
+    if(strcmp(commands,"pwd") != 10 && strcmp(commands,"ls") != 10 && strcmp(commands,"exit") != 10 && strcmp(commands,"help") != 10){
         printf("command not found\ntype 'help' for informations\n");
     }
 }
@@ -56,7 +61,24 @@ void get_current_dir(){
 
 void help_option(){
     if(strcmp(commands,"help") == 10){
-        printf("Commands implemented:\n'exit'- leave microshell\n'pwd' - show current directory");
+        printf("Commands implemented:\n'exit'- leave microshell\n'pwd' - show current directory\n");
+        printf("Autor Jan Kordas\nWydzial informatyki i informatyki UAM Poznan\n");
     }
+}
 
+void display_files(){
+    if(strcmp(commands,"ls") == 10){
+        DIR *dir;
+        struct dirent *names_of_dirs;
+        dir = opendir(".");
+        if(dir == NULL){
+            printf("Error in opening current dir");
+        }
+        while((names_of_dirs = readdir(dir)) != NULL){
+            if((names_of_dirs -> d_type == DT_REG) || (names_of_dirs -> d_type == DT_DIR)){
+                printf("%s\n", names_of_dirs -> d_name);
+        }
+    }
+     closedir(dir);
+}
 }
