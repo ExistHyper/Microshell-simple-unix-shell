8f440186fa3341728bcd83bdd8a11398d5bbabdd
ExistHyper <jan.kordas90@gmail.com>
2022-12-13 12:22:55 +0100

    change in parsing added whoami hostname

--- microshell.c
+++ microshell.c
@@ -7,50 +7,78 @@
 #include <sys/types.h>
 #include <signal.h>
 
-char commands[40];
+char arr_of_commands[][32] = {"hostname", "whoami", "exit", "pwd", "help", "ls" };
+char commands[32];
+void get_hostname();
+void get_login();
 void get_current_dir();
 void exit_function();
 void unknown_command_error();
 void help_option();
 void display_files();
 
+
+
 int main(){
-    char hostname[_SC_HOST_NAME_MAX];
-    hostname[39] = '\0';
-    gethostname(hostname,40);
     printf("****WELCOME TO MICROSHELL****\n");
     printf("\n");
 
     while(1){
-        printf("%s@%s:~$",getlogin(),hostname);
-        fgets(commands, 40, stdin);
+        char current_dir[PATH_MAX];
+        getcwd(current_dir, PATH_MAX);
+        printf("[%s] $ ", current_dir);
+        fgets(commands, 32, stdin);
+        commands[strlen(commands) - 1] = 0; //usuwanie znaku konca stringu
         
-        
+
+
+        get_hostname();
+        get_login();
         get_current_dir();
         exit_function();
         unknown_command_error();
         help_option();
         display_files();
 
-        //printf("%s", commands);
-        //printf("\n%d\n", strcmp(commands,"exit"));
     }
 }
-void unknown_command_error(){
-    if(strcmp(commands,"pwd") != 10 && strcmp(commands,"ls") != 10 && strcmp(commands,"exit") != 10 && strcmp(commands,"help") != 10){
-        printf("command not found\ntype 'help' for informations\n");
+void get_hostname(){
+    if(strcmp(commands,"hostname") == 0){
+    char hostname[_SC_HOST_NAME_MAX];
+    //hostname[39] = '\0';
+    gethostname(hostname,40);
+    printf("%s\n", hostname); 
+    }
+    
+}
+void get_login(){
+    if(strcmp(commands,"whoami") == 0){
+        printf("%s\n",getlogin());
     }
 }
 
+void unknown_command_error(){
+    int flag = 0;
+    for(int i = 0; i < 6; i++){
+        if(strcmp(arr_of_commands[i], commands) == 0){
+            flag = 1;
+        }
+    }
+    if(flag == 0){
+        printf("command not found\ntype 'help' for informations\n");
+    }
+    
+}
+
 void exit_function(){
-    if(strcmp(commands,"exit") == 10){
+    if(strcmp(commands,"exit") == 0){
             exit(1);
     }
     
 }
 
 void get_current_dir(){
-    if(strcmp(commands,"pwd") == 10){
+    if(strcmp(commands,"pwd") == 0){
         char current_dir[PATH_MAX];
         getcwd(current_dir, PATH_MAX);
         printf("%s\n",current_dir);
@@ -60,14 +88,20 @@ void get_current_dir(){
 }
 
 void help_option(){
-    if(strcmp(commands,"help") == 10){
-        printf("Commands implemented:\n'exit'- leave microshell\n'pwd' - show current directory\n");
+    if(strcmp(commands,"help") == 0){
+        printf("List of commands:\n
+        'exit'- leave microshell\n'
+        'pwd' - display current directory\n
+        'whoami' - display login
+        'hostname' - display hostname
+        
+        ");
         printf("Autor Jan Kordas\nWydzial informatyki i informatyki UAM Poznan\n");
     }
 }
 
 void display_files(){
-    if(strcmp(commands,"ls") == 10){
+    if(strcmp(commands,"ls") == 0){
         DIR *dir;
         struct dirent *names_of_dirs;
         dir = opendir(".");
@@ -81,4 +115,6 @@ void display_files(){
     }
      closedir(dir);
 }
+
+
 }
