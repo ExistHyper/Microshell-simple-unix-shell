8e668f6d7ca91e1a5618945d9ed7c336ad49bebf
ExistHyper <jan.kordas90@gmail.com>
2022-12-20 10:13:48 +0100

    working fork but not for cd

--- microshell_cwiczenia.c
+++ microshell_cwiczenia.c
@@ -3,23 +3,30 @@
 #include <sys/wait.h>
 #include <string.h>
 int main(){
-    //use strtok
     while(1){
         char argument_list[32];
-        char *buf;
-        char commands[32];
+        char input[32];
+        char *commands[32];
+        int i = 0;
+        char *parameter_to_pass[32];
+        char delimiter[] = " \n\t";
         printf("\ntype $ ");
-        fgets(commands, 32, stdin);
-        buf = strtok(commands, argument_list);
-        commands[strlen(commands) - 1] = 0;
+        fgets(input, 32, stdin);
+        input[strlen(input) - 1] = 0;
+        char *token_command = strtok(input, delimiter);
+        while(token_command != NULL){
+            commands[i++] = token_command;
+            token_command = strtok(NULL,delimiter);
+        }
+        commands[i++] = NULL;
+        
         pid_t id = fork();
         if(id == 0){
-            execvp(commands, argument_list);
+            execvp(commands[0], commands);
         }
         else{
             wait(NULL);
-            printf("parent process\n");
+            
         }
-        printf("%s\n", commands);
     }
 }
